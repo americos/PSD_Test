ffdd328c52d73f7ebc84a7b2304c54a1dede5691
Americo Savinon <americos@gmail.com>
2012-10-22 01:58:38 -0400

    Adding js file, tab functionality, links and submit form

---
+++ js/app.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function() {
+	
+	//Tab Functionality: Click on Menu links
+	$(".menu a").click(function(e){
+
+		e.preventDefault();
+
+		//Remove active class
+		$(".menu li").removeClass("active");
+
+		//Add active class to element clicked
+		var link = $(this);
+		link.parent().addClass("active");
+
+		var tab_to_activate = link.attr("elem");
+		
+		// Show/Hide Tabs
+		$(".tabs").hide();
+		$("#" + tab_to_activate).show();
+
+	});
+
+});
